3b349d3a60119eeefc75d6d468e989eb34cfa6f5
geraldotomaz <gtsilva90@hotmail.com>
2016-10-29 14:08:20 -0200

    Update TvShow.py
    Review feedback

--- TvShow.py
+++ TvShow.py
@@ -1,6 +1,7 @@
 from video import Video
 
 class TvShow(Video):
+	"""Class TvShow.py for category implementation"""
     def __init__(self, title, duration, category, trailer_youtube_url, poster_image_url, season, episode, tv_station):
         Video.__init__(self, title, duration, category, trailer_youtube_url, poster_image_url)
         self.season = season
