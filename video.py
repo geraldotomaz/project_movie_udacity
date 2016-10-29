33fe24ba7cd1418b90804ed4a2ddd102d63a6639
geraldotomaz <gtsilva90@hotmail.com>
2016-10-29 14:09:59 -0200

    Update video.py
    Review feedback

--- video.py
+++ video.py
@@ -1,4 +1,5 @@
 class Video():
+	"""Is class Father for media.py and TvShow.py"""
     def __init__(self, title, duration, category, trailer_youtube_url, poster_image_url):
         print("Instance class video")
         self.title = title
@@ -7,3 +8,4 @@ class Video():
         self.trailer_youtube_url = trailer_youtube_url
         self.poster_image_url = poster_image_url
         
+
