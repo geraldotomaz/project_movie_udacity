24b7e1858d44faa78a6f0e4bfc31d34c1649950a
geraldotomaz <gtsilva90@hotmail.com>
2016-10-29 14:11:00 -0200

    Update media.py

--- media.py
+++ media.py
@@ -1,8 +1,8 @@
 from video import Video
 
 class Movie(Video):
+	"""Class media.py for category implementation"""
     def __init__(self, title, duration, category, trailer_youtube, poster_image_url, movie_storyline):
         Video.__init__(self, title, duration, category, trailer_youtube, poster_image_url)
         self.movie_storyline = movie_storyline
  
-
